c2f3565ecb20e434c19d70614d50eb98cb3a9188
Liudmila <ludo4ka.th@mail.ru>
2021-02-21 18:21:00 +0700

    Переименованы переменные

--- module_3/test_sample.py
+++ module_3/test_sample.py
@@ -11,10 +11,10 @@ from selenium import webdriver
 
 main_page_link = "http://selenium1py.pythonanywhere.com/ru/"
 
-press_login_button_on_main_page = "#login_link"
-input_login_mail = "[name='login-username']"
-input_login_password = "[name='login-password']"
-login = "[name='login_submit']"
+login_btn_on_main_page = "#login_link"
+login_mail_field = "[name='login-username']"
+login_password_field = "[name='login-password']"
+login_btn_on_login_page = "[name='login_submit']"
 greeting_alert = ".alertinner"
 
 
@@ -28,7 +28,7 @@ def test_go_to_login_page():
         browser.get(main_page_link)
 
         # Act
-        browser.find_element_by_css_selector(press_login_button_on_main_page).click()
+        browser.find_element_by_css_selector(login_btn_on_main_page).click()
 
         # Assert
         assert browser.current_url == login_page_url, "User is redirected to the wrong page"
@@ -48,19 +48,19 @@ def test_user_login_succesfully():
         browser = webdriver.Chrome()
         browser.implicitly_wait(6)
         browser.get(main_page_link)
-        browser.find_element_by_css_selector(press_login_button_on_main_page).click()
+        browser.find_element_by_css_selector(login_btn_on_main_page).click()
 
         # Act
-        mail_field = browser.find_element_by_css_selector(input_login_mail)
-        mail_field.click()
-        mail_field.send_keys(mail)
+        mail_input = browser.find_element_by_css_selector(login_mail_field)
+        mail_input.click()
+        mail_input.send_keys(mail)
 
-        password_field = browser.find_element_by_css_selector(input_login_password)
-        password_field.click()
-        password_field.send_keys(password)
+        password_input = browser.find_element_by_css_selector(login_password_field)
+        password_input.click()
+        password_input.send_keys(password)
 
-        log_in = browser.find_element_by_css_selector(login)
-        log_in.click()
+        log_in_btn = browser.find_element_by_css_selector(login_btn_on_login_page)
+        log_in_btn.click()
 
         # Assert
         greeting_alert_after_login = browser.find_element_by_css_selector(greeting_alert)
